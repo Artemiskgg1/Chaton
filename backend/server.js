05b06f084462b7eb5ecbc4eab45f5bcad4deb03e
Abha Ghildiyal <100835504+AbhaGhildiyal@users.noreply.github.com>
2024-03-06 22:09:24 +0530

    Add message routes and update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
-import authRoutes from "./routes/message.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectDB from "./db/connect.js";
 
 const app = express();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 dotenv.config();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
