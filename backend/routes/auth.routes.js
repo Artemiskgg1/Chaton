6b807a233190dab093e6b1a3dea7bcfef35fd124
Abha Ghildiyal <100835504+AbhaGhildiyal@users.noreply.github.com>
2024-03-06 21:15:07 +0530

    Add dependencies and update routes in server.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,7 +1,7 @@
-import express from "express";
+import { Router } from "express";
 import { login, logout, signup } from "../controllers/auth.controllers.js";
-const router = express.Router();
-router.post("/login", signup);
-router.post("/signup", login);
+const router = Router();
+router.post("/signup", signup);
+router.post("/login", login);
 router.post("/logout", logout);
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
+import authRoutes from "./routes/message.routes.js";
+import connectDB from "./db/connect.js";
 
 const app = express();
-dotenv.config();
 const PORT = process.env.PORT || 3000;
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+
+dotenv.config();
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+  console.log(`Server is running on PORT: ${PORT}`);
 });
